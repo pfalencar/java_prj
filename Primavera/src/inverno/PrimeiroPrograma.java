765a8e24f097f385025deddc0bea4f3c98267816
pfalencar <patricia.alencar@etec.sp.gov.br>
2018-08-14 15:04:45 -0300

    Print e concatenacao

---
+++ Primavera/src/inverno/PrimeiroPrograma.java
@@ -0,0 +1,23 @@
+package inverno;
+
+public class PrimeiroPrograma {
+	public static void main(String[] args) {
+	
+		int numero0 = 0;
+		int numero1 = 1;
+		int numero2 = 2;
+		int numero3 = 3;
+		int numero4 = 4;
+		int numero5 = 5;
+		int numero6 = 6;
+		int numero7 = 7;
+		int numero8 = 8;
+		int numero9 = 9;
+		int numro10 = 10;
+		
+		System.out.println("2x0=" + numero2 * numero0);
+		
+		System.out.print(numero2 + "x" + numero1 + "=" + numero2 * numero1);
+	}
+		
+}
